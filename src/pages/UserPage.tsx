47aa7d491e9a195654f5ca784359b60fb1171760
rushikeshmane <rushikeshmane401@gmail.com>
2025-08-17 11:54:20 +0530

    Test 11111111111111111111111111111111111111111111111111111111111111111111111111111111

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -5,7 +5,7 @@ const UserDetails = () => {
   const email = searchParams.get("email");
 
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-100">
+    <div className="flex h-screen  items-center justify-center bg-gray-100">
       <h1 className="text-2xl font-bold text-gray-800">
         Hello {email ? email : "Guest"}
       </h1>
